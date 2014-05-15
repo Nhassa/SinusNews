e3d5023bdc39a6f1fae1e814fc0c4accbf617247
Nhassa <devyatovanm@bk.ru>
2014-05-15 17:59:48 +0500

    Add news' dates and ids, style list. Set onItemClickListener.

--- SinusNews/gen/com/example/sinusnews/R.java
+++ SinusNews/gen/com/example/sinusnews/R.java
@@ -14,10 +14,12 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070006;
+        public static final int menu_settings=0x7f070008;
         public static final int newsListView=0x7f070002;
         public static final int progressBar1=0x7f070003;
-        public static final int tvDesc=0x7f070005;
+        public static final int tvDate=0x7f070007;
+        public static final int tvDesc=0x7f070006;
+        public static final int tvId=0x7f070005;
         public static final int tvInfo=0x7f070000;
         public static final int tvResult=0x7f070001;
         public static final int tvTitle=0x7f070004;
@@ -33,9 +35,14 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int header_text=0x7f040007;
         public static final int hello_world=0x7f040001;
         public static final int load_text=0x7f040003;
         public static final int menu_settings=0x7f040002;
+        public static final int news_date=0x7f040006;
+        public static final int news_desc=0x7f040005;
+        public static final int news_id=0x7f040008;
+        public static final int news_title=0x7f040004;
     }
     public static final class style {
         /** 
--- SinusNews/src/com/example/sinusnews/MainActivity.java
+++ SinusNews/src/com/example/sinusnews/MainActivity.java
@@ -26,6 +26,7 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.*;
 import android.widget.AbsListView.OnScrollListener;
+import android.widget.AdapterView.OnItemClickListener;
 import android.net.ConnectivityManager; 
 import android.net.NetworkInfo; 
 import android.content.Context; 
@@ -33,14 +34,18 @@ import android.content.Context;
 public class MainActivity extends Activity {
 
 	final String LOG_TAG = "sinusLogs";
+	final String ATTR_NAME_ID = "post_id";
 	final String ATTR_NAME_TITLE = "post_title";
 	final String ATTR_NAME_DESC = "post_content";
+	final String ATTR_NAME_DATE = "post_date";
 	
 	TextView tvInfo;
 	TextView tvResult;
 	ListView lvResult;
+	TextView tvId;
 	TextView tvTitle;
 	TextView tvDesc;
+	TextView tvDate;
 	AsyncNewsLoader loader;
 	//ArrayList<String> massive;
 	//ArrayList<Map<String, String>> result;
@@ -62,8 +67,10 @@ public class MainActivity extends Activity {
 		 tvInfo = (TextView) findViewById(R.id.tvInfo);
 		 tvResult = (TextView) findViewById(R.id.tvResult);
 		 lvResult = (ListView) findViewById(R.id.newsListView);
+		 tvId = (TextView) findViewById(R.id.tvId);
 		 tvTitle = (TextView) findViewById(R.id.tvTitle);
 		 tvDesc = (TextView) findViewById(R.id.tvDesc);
+		 tvDate = (TextView) findViewById(R.id.tvDate);
 		 
 		 //massive = new ArrayList<String>();
 		 
@@ -92,10 +99,17 @@ public class MainActivity extends Activity {
 		 m.put(ATTR_NAME_DESC, massive);
 		 massive_map.add(m);*/
 		 
-		 String[] from = {ATTR_NAME_TITLE, ATTR_NAME_DESC};
-		 int[] to = {R.id.tvTitle, R.id.tvDesc};
+		 String[] from = {ATTR_NAME_ID, ATTR_NAME_TITLE, ATTR_NAME_DESC, ATTR_NAME_DATE};
+		 int[] to = {R.id.tvId, R.id.tvTitle, R.id.tvDesc, R.id.tvDate};
 		 
-		 sAdapter = new SimpleAdapter(this, massive_map, R.layout.list_item_map, from, to);
+		 sAdapter = new SimpleAdapter(this, massive_map, R.layout.list_item_map, from, to){
+			 //long newsId;
+			 /*public long getItemId(int position){
+				 
+				 return position;
+			 }*/
+		 };
+		 
 		 
 		 //Footer
 		 //footer = getLayoutInflater().inflate(R.layout.footer, null);
@@ -131,6 +145,16 @@ public class MainActivity extends Activity {
 							toast.show();
 					 }
 	             }
+			 }
+		 });
+		 
+		 lvResult.setOnItemClickListener( new OnItemClickListener(){
+			 public void onItemClick(AdapterView<?> parent, View view,
+			          int position, long id)
+			 {
+				 TextView tv = (TextView) view.findViewById(R.id.tvId);
+				 String str = tv.getText().toString();
+				 Log.d(LOG_TAG, "itemClick: position = " + position + ", id = " + str);
 			 }
 		 });
 		
@@ -173,7 +197,7 @@ public class MainActivity extends Activity {
 	private class AsyncNewsLoader extends AsyncTask<Integer, Void, String> {
 		protected void onPreExecute() {
 			super.onPreExecute();
-		      	tvInfo.setText("Latest news: загружаются...");
+		      	tvInfo.setText("Новости: загружаются...");
 		      	Log.d(LOG_TAG, "startLoading");
 		}
 		
@@ -217,7 +241,7 @@ public class MainActivity extends Activity {
 		
 		protected void onPostExecute(String json_txt) {
 			super.onPostExecute(json_txt);
-		      	tvInfo.setText("Latest news:");
+		      	tvInfo.setText("Новости:");
 		      	//tvResult.setText(json_txt);
 		      	
 		      	Log.d(LOG_TAG, "endLoading");
@@ -229,8 +253,10 @@ public class MainActivity extends Activity {
 					for(int i=0; i<ja.length(); i++){
 						JSONObject jo = ja.getJSONObject(i);
 						m = new HashMap<String, Object>();
+						m.put(ATTR_NAME_ID, jo.getString("ID"));
 						m.put(ATTR_NAME_TITLE, jo.getString("post_title"));
 						m.put(ATTR_NAME_DESC, jo.getString("post_content").replaceAll("\\<[^>]*>", ""));
+						m.put(ATTR_NAME_DATE, jo.getString("post_date"));
 						massive_map.add(m);
 						//massive.add(jo.getString("post_title"));
 					}
